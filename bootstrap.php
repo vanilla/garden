6ad8355fd8a872a8bb0a695374133592d2dfce49
Todd Burry <todd@vanillaforums.com>
2013-03-16 18:26:06 -0400

    Added the feature() function to the framework bootstrap.

--- bootstrap.php
+++ bootstrap.php
@@ -6,3 +6,16 @@ require_once __DIR__.'/functions.core.php';
 require_once __DIR__.'/functions.error.php';
 
 spl_autoload_register('autoloadDir');
+
+function feature($name, $force = true) {
+   switch (strtolower($name)) {
+      case 'commandline':
+         if (PHP_SAPI !== 'cli')
+            trigger_error("This script must be called from the command line.", E_USER_ERROR);
+         
+         require_once __DIR__.'/functions.commandline.php';
+         break;
+      default:
+         trigger_error("Unknown feature: $name.", E_USER_ERROR);
+   }
+}
