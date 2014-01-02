46f2026cfd7b89f0eb9f3c0271e958f9571beb31
Todd Burry <todd@vanillaforums.com>
2014-01-02 17:22:34 -0500

    Added some documentation to autoload_files.php.

--- autoload_files.php
+++ autoload_files.php
@@ -1,9 +1,23 @@
 <?php
 
+/*
+ * Includes the appropriate function and constant files necessary to run the vanilla framework.
+ *
+ * This file must be included *after* the framework has been registered with an autoloader.
+ *
+ * @author Todd Burry <todd@vanillaforums.com>
+ * @copyright 2009 Vanilla Forums Inc.
+ * @license LGPL-2.1
+ * @package Vanilla
+ * @since 1.0
+ */
+
+use \Vanilla\Event;
+
 // Include the core functions.
 require_once __DIR__.'/functions/core-functions.php';
 
-// Load the framework's overrideable functions as late as possible so that addons can override them.
+// Load the framework's overrideable functions late so that addons can override them.
 Event::bind('framework_loaded', function() {
     require_once __DIR__.'/functions/formatting-functions.php';
 }, Event::PRIORITY_LOW);
