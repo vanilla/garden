91883c735c5f5441f603512f36f7f9bb68cf88c9
Todd Burry <todd@vanillaforums.com>
2014-01-02 15:54:15 -0500

    Change functions.php into autoload_files.php to mirror the composer autoloader filenames. Added some doc comments to the autoloaders.

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-framework
-=========
+# The Vanilla Framework
--- autoload.php
+++ autoload.php
@@ -1,17 +1,36 @@
 <?php
 
-use \Vanilla\Event;
+/*
+ * Defines and registers an autoloader for the vanilla framework.
+ *
+ * Include this file if you are not using another autoloader such as the autoloader build with composer.
+ *
+ * @author Todd Burry <todd@vanillaforums.com>
+ * @copyright 2009 Vanilla Forums Inc.
+ * @license LGPL-2.1
+ * @package Vanilla
+ * @since 1.0
+ */
 
-function autoload_vanilla($className) {
-    $className = ltrim($className, '\\');
+/**
+ * A psr-0 compatible autoloader for the Vanilla framework.
+ * @param string $class_name
+ */
+function autoload_vanilla($class_name) {
+    $class_name = ltrim($class_name, '\\');
+
+    // Only load classes in the Vanilla\ namespace.
+    if (strpos($class_name, 'Vanilla\\') !== 0)
+        return;
+
     $fileName  = '';
     $namespace = '';
-    if ($lastNsPos = strrpos($className, '\\')) {
-        $namespace = substr($className, 0, $lastNsPos);
-        $className = substr($className, $lastNsPos + 1);
+    if ($lastNsPos = strrpos($class_name, '\\')) {
+        $namespace = substr($class_name, 0, $lastNsPos);
+        $class_name = substr($class_name, $lastNsPos + 1);
         $fileName  = str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
     }
-    $fileName .= str_replace('_', DIRECTORY_SEPARATOR, $className) . '.php';
+    $fileName .= str_replace('_', DIRECTORY_SEPARATOR, $class_name) . '.php';
     $fileName = __DIR__.'/'.$fileName;
 
     if (file_exists($fileName)) {
@@ -20,4 +39,5 @@ function autoload_vanilla($className) {
 }
 spl_autoload_register('autoload_vanilla');
 
-require_once __DIR__.'/functions.php';
+// The autoload files must be loaded after the autoloader.
+require_once __DIR__.'/autoload_files.php';
