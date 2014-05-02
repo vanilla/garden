34b8a5c0486b106d5dd60fac0d15c46c386d08b4
Todd Burry <todd@vanillaforums.com>
2014-05-02 18:21:57 -0400

    Move the file autoloader.

--- src/functions/autoload.php
+++ src/functions/autoload.php
@@ -7,7 +7,7 @@
  *
  * @author Todd Burry <todd@vanillaforums.com>
  * @copyright 2009 Vanilla Forums Inc.
- * @license LGPL-2.1
+ * @license MIT
  * @package Vanilla
  * @since 1.0
  */
@@ -15,9 +15,9 @@
 use \Garden\Event;
 
 // Include the core functions.
-require_once __DIR__ . '/functions/core-functions.php';
+require_once __DIR__.'/core-functions.php';
 
 // Load the framework's overrideable functions late so that addons can override them.
-Event::bind('bootstrap', function() {
-    require_once __DIR__ . '/functions/formatting-functions.php';
+Event::bind('bootstrap', function () {
+    require_once __DIR__.'/formatting-functions.php';
 }, Event::PRIORITY_LOW);
