54bbe3c672ab99dfcb86d8dc0474b2324040b572
Todd Burry <todd@vanillaforums.com>
2014-06-18 11:58:42 -0400

    Fix the doc comments on some classes.

--- src/Exception/ClientException.php
+++ src/Exception/ClientException.php
@@ -1,4 +1,9 @@
 <?php
+/**
+ * @author Todd Burry <todd@vanillaforums.com>
+ * @copyright 2009-2014 Vanilla Forums Inc.
+ * @license MIT
+ */
 
 namespace Garden\Exception;
 
--- src/Exception/ServerException.php
+++ src/Exception/ServerException.php
@@ -1,13 +1,14 @@
 <?php
+/**
+ * @author Todd Burry <todd@vanillaforums.com>
+ * @copyright 2009-2014 Vanilla Forums Inc.
+ * @license MIT
+ */
 
 namespace Garden\Exception;
 
 /**
  * Represents a 5xx server exception.
  */
-
 class ServerException extends \Exception {
 }
-
-
-
